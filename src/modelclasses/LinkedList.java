262cc637fe1e3f8c1105d8e11bfd1e5a02f09837
Young <andreasyoungandersen@gmail.com>
2022-03-10 19:39:09 +0100

    LinkedList implemented

--- src/modelclasses/LinkedList.java
+++ src/modelclasses/LinkedList.java
@@ -1,6 +1,40 @@
 package modelclasses;
 
-import java.util.List;
+import java.util.EmptyStackException;
 
-public class LinkedList<T> extends List<T> {
+public class LinkedList<T> implements MyList<T> {
+	private Node<T> head;
+	private int size;
+
+	public LinkedList() {
+		size = 0;
+	}
+
+	@Override
+	public void addToFront(T data) {
+		head = (Node<T>) new Node(data, head);
+		size++;
+	}
+
+	@Override
+	public int getSize() {
+		return size;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return size == 0;
+	}
+
+	@Override
+	public T removeFirst() throws EmptyStackException {
+		if (isEmpty()) {
+			throw new EmptyStackException();
+		} else {
+			Node n = head;
+			head = head.getNext();
+			size--;
+			return (T) n.getData();
+		}
+	}
 }
