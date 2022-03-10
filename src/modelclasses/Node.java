4e5a2f36ec7c2ffd3678506eb2b77971b41b5558
Young <andreasyoungandersen@gmail.com>
2022-03-10 19:30:02 +0100

    Constructor added

--- src/modelclasses/Node.java
+++ src/modelclasses/Node.java
@@ -4,6 +4,11 @@ public class Node<T> {
 	private T data;
 	private Node<T> nextNode;
 
+	public Node(T data, Node<T> nextNode) {
+		this.data = data;
+		this.nextNode = nextNode;
+	}
+
 	public void setData(T data){
 		this.data = data;
 	}
