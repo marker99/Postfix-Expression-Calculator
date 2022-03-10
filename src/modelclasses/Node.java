b99ca0b05758cc6b60220d832eaa4520b3bf2a91
Young <andreasyoungandersen@gmail.com>
2022-03-10 19:16:18 +0100

    Node methods implemented

--- src/modelclasses/Node.java
+++ src/modelclasses/Node.java
@@ -1,4 +1,22 @@
 package modelclasses;
 
 public class Node<T> {
+	private T data;
+	private Node<T> nextNode;
+
+	public void setData(T data){
+		this.data = data;
+	}
+
+	public T getData(){
+		return data;
+	}
+
+	public void setNext(Node<T> node){
+		nextNode = node;
+	}
+
+	public Node<T> getNext() {
+		return nextNode;
+	}
 }
