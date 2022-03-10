5ef8e3d4dc80ba3d2aa4e2eb173a391127fba735
Frederik <frederik.m.hansen@gmail.com>
2022-03-10 19:19:44 +0100

    more again

--- src/CalculatorVisitor.java
+++ src/CalculatorVisitor.java
@@ -1,8 +1,9 @@
+import modelclasses.LinkedStack;
+
 public class CalculatorVisitor implements Visitor, Calculator
 {
 
-
-
+    private LinkedStack<Token> tokenStack;
 
     @Override public void visit(Operand operand)
     {
@@ -17,5 +18,15 @@ public class CalculatorVisitor implements Visitor, Calculator
     @Override public int getResult()
     {
         return 0;
+    }
+
+    private void pushOperand(Operand operand)
+    {
+
+    }
+
+    private void performOperation(Operator operator)
+    {
+
     }
 }
--- src/Operand.java
+++ src/Operand.java
@@ -1,3 +1,16 @@
 public class Operand extends Token
 {
+    private Operation operation;
+
+    @Override public void accept(CalculatorVisitor visitor)
+    {
+        super.accept(visitor);
+    }
+
+    public int getValue()
+    {
+        return 0;
+    }
+
+
 }
