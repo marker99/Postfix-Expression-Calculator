1bd748ff5b15fe88a4e1b09bf8e0b7acc2e422fe
SanderKirchert <Sanders@post.cybercity.dk>
2022-03-10 20:41:46 +0100

    implemented CalculatorVisitor.getResult

--- src/calculator/CalculatorVisitor.java
+++ src/calculator/CalculatorVisitor.java
@@ -20,7 +20,7 @@ public class CalculatorVisitor implements Visitor, Calculator
 
     @Override public int getResult()
     {
-        return 0;
+        return ((Operand) tokenStack.pop()).getValue();
     }
 
     private void pushOperand(Operand operand)
